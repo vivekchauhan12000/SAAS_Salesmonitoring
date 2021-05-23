7aa5f5bca44facc4f27226165797316cc6de7939
vivekchauhan12000 <vivekchauhan12000@gmail.com>
2021-05-23 16:35:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose=require("mongoose");
+
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://vivek:vivek@cluster0.9pguw.mongodb.net/sales?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+  console.log("MongoDb connected");
+}).catch((err)=>
+console.log(err)
+);
+
+const PORT=process.env.PORT || 8080;
+
+
+
+app.listen(PORT,()=>{
+  console.log("server running")
+})
